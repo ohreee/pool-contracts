1b1834317d8597c0216e618c008a66dd2df1b118
Eddine Omar <eddine.farid@hotmail.fr>
2021-02-17 22:20:33 +0100

    update tests

--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -1,103 +1,129 @@
+const { assert } = require('chai');
+var Web3 = require('web3');
+
 var SimpleBank = artifacts.require("./SimpleBank.sol");
 
-const ether = 10**18; // 1 ether = 1000000000000000000 wei
-const reward = 10 * ether;
-const initialDepositsBalance = 30 * ether;
+const ether = 10 ** 18; // 1 ether = 1000000000000000000 wei
+const wei = 1;
+const initialDepositsBalance = 1 * ether;
 
-contract("SimpleBank - basic initialization", function(accounts) {
+contract("SimpleBank - basic initialization", function (accounts) {
+  const chairperson = accounts[0];
   const alice = accounts[1];
   const bob = accounts[2];
-  const charlie = accounts[3];
-  const dave = accounts[4];
+  const eddine = accounts[3];
+  const aslan = accounts[4];
 
-  it("should reward 3 first clients with 10 balance", async () => {
-    const bank = await SimpleBank.deployed();
+  it("enroll everyone", async () => {
+    const bank = await SimpleBank.deployed({ from: chairperson});
+    var chairpersonBalanceWallet = await this.web3.eth.getBalance(chairperson);
+    // var gasPrice = await this.web3.eth.getGasPrice();
+    // assert.equal(chairpersonBalanceWallet, Web3.utils.toWei("99", "ether") - bank.gas * gasPrice);
 
-    await bank.enroll({from: alice});
-    const aliceBalance = await bank.balance({from: alice});
-    assert.equal(aliceBalance, reward, "initial balance is incorrect");
+    await bank.enroll(alice, { from: chairperson });
+    const aliceBalance = await bank.balance({ from: alice });
+    assert.equal(aliceBalance.toNumber(), 0, "initial balance is incorrect");
 
-    await bank.enroll({from: bob});
-    const bobBalance = await bank.balance({from: bob});
-    assert.equal(bobBalance, reward, "initial balance is incorrect");
+    await bank.enroll(bob, { from: chairperson });
+    const bobBalance = await bank.balance({ from: bob });
+    assert.equal(bobBalance, 0, "initial balance is incorrect");
 
-    await bank.enroll({from: charlie});
-    const charlieBalance = await bank.balance({from: charlie});
-    assert.equal(charlieBalance, reward, "initial balance is incorrect");
+    await bank.enroll(eddine, { from: chairperson });
+    const eddineBalance = await bank.balance({ from: eddine });
+    assert.equal(eddineBalance, 0, "initial balance is incorrect");
 
-    await bank.enroll({from: dave});
-    const daveBalance = await bank.balance({from: dave});
-    assert.equal(daveBalance, 0, "initial balance is incorrect");
+    await bank.enroll(aslan, { from: chairperson });
+    const aslanBalance = await bank.balance({ from: aslan });
+    assert.equal(aslanBalance, 0, "initial balance is incorrect");
 
-    const depositsBalance = await bank.depositsBalance();
-    assert.equal(depositsBalance, initialDepositsBalance, "initial balance is incorrect");
+    // const depositsBalance = await bank.depositsBalance();
+    // assert.equal(depositsBalance.toNumber(), initialDepositsBalance, "initial balance is incorrect");
   });
 
   it("should deposit correct amount", async () => {
-    const bank = await SimpleBank.deployed();
-    const deposit = 2 * ether;
+    const bank = await SimpleBank.deployed({ from: chairperson});
+    const deposit = 1.5 * ether;
 
-    const receipt = await bank.deposit({from: alice, value: web3.utils.toBN(deposit)});
+    const receipt = await bank.deposit({ from: alice, value: web3.utils.toBN(deposit) });
 
-    const balance = await bank.balance({from: alice});
-    assert.equal(balance, reward + deposit,
-        "deposit amount incorrect, check deposit method");
-    const depositsBalance = await bank.depositsBalance();
-    assert.equal(depositsBalance, initialDepositsBalance + deposit,
-        "bank deposits balance should be increased");
+    const balance = await bank.balance({ from: alice });
+    assert.equal(balance, deposit,
+      "deposit amount incorrect, check deposit method");
+    //   const depositsBalance = await bank.depositsBalance();
+    //   assert.equal(depositsBalance, initialDepositsBalance + deposit,
+    //     "bank deposits balance should be increased");
 
-    const expectedEventResult = {accountAddress: alice, amount: deposit};
+    const expectedEventResult = { accountAddress: alice, amount: deposit };
     assert.equal(receipt.logs[0].args.accountAddress, expectedEventResult.accountAddress,
-        "LogDepositMade event accountAddress property not emitted");
+      "LogDepositMade event accountAddress property not emitted");
     assert.equal(receipt.logs[0].args.amount, expectedEventResult.amount,
-        "LogDepositMade event amount property not emitted");
+      "LogDepositMade event amount property not emitted");
   });
 });
 
-contract("SimpleBank - proper withdrawal", function(accounts) {
+contract("SimpleBank - proper withdrawal", function (accounts) {
+  const chairperson = accounts[0];
   const alice = accounts[1];
+  const bob = accounts[2];
+  const eddine = accounts[3];
+  const aslan = accounts[4];
 
   it("should withdraw correct amount", async () => {
-    const bank = await SimpleBank.deployed();
+    const bank = await SimpleBank.deployed({ from: chairperson});
     const deposit = 5 * ether;
 
-    await bank.deposit({from: alice, value: web3.utils.toBN(deposit)});
-    await bank.withdraw(web3.utils.toBN(deposit), {from: alice});
+    await bank.enroll(alice, { from: chairperson });
+    const receipt = await bank.deposit({ from: alice, value: web3.utils.toBN(deposit) });
+    const balance = await bank.balance({ from: alice });
+    assert.equal(balance, deposit,
+      "deposit amount incorrect, check deposit method");
 
-    const balance = await bank.balance({from: alice});
-    assert.equal(balance, deposit - deposit, "withdraw amount incorrect");
+    await bank.withdraw(web3.utils.toBN(deposit), { from: alice });
+
+    const new_balance = await bank.balance({ from: alice });
+    assert.equal(new_balance, 0, "withdraw amount incorrect");
   });
 });
 
-contract("SimpleBank - incorrect withdrawal", function(accounts) {
+contract("SimpleBank - incorrect withdrawal", function (accounts) {
+  const chairperson = accounts[0];
   const alice = accounts[1];
 
-  it("should keep balance unchanged if withdraw greater than balance", async() => {
-    const bank = await SimpleBank.deployed();
+  it("should keep balance unchanged if withdraw greater than balance", async () => {
+    const bank = await SimpleBank.deployed({ from: chairperson});
     const deposit = 3 * ether;
 
-    await bank.deposit({from: alice, value: web3.utils.toBN(deposit)});
-    await bank.withdraw(web3.utils.toBN(deposit + 1*ether), {from: alice});
+    await bank.enroll(alice, { from: chairperson });
+    await bank.deposit({ from: alice, value: web3.utils.toBN(deposit) });
+    try {
+      await bank.withdraw(web3.utils.toBN(deposit + 1 * ether), { from: alice });
+    } catch (e) {
+      assert(e, "Error: VM Exception while processing transaction: revert");
+    }
 
-    const balance = await bank.balance({from: alice});
+    const balance = await bank.balance({ from: alice });
     assert.equal(balance, deposit, "balance should be kept intact");
   });
 });
 
-contract("SimpleBank - fallback works", function(accounts) {
+contract("SimpleBank - not enrolled - fallback works", function (accounts) {
+  const chairperson = accounts[0];
   const alice = accounts[1];
 
-  it("should revert ether sent to this contract through fallback", async() => {
-    const bank = await SimpleBank.deployed();
+  it("should revert ether sent to this contract through fallback", async () => {
+    const bank = await SimpleBank.deployed({ from: chairperson});
     const deposit = 3 * ether;
 
+    const first_balance = await this.web3.eth.getBalance(alice);
+
     try {
-      await bank.send(web3.utils.toBN(deposit), {from: alice});
-    } catch(e) {
+      await bank.deposit({ from: alice, value: web3.utils.toBN(deposit) });
+    } catch (e) {
       assert(e, "Error: VM Exception while processing transaction: revert");
     }
 
-    const depositsBalance = await bank.depositsBalance();
-    assert.equal(depositsBalance, initialDepositsBalance, "balance should be kept intact");
+    const second_balance = await this.web3.eth.getBalance(alice);
+    assert.approximately(parseFloat(Web3.utils.fromWei(first_balance)), parseFloat(Web3.utils.fromWei(second_balance)), 1, "Alice balance should be kept intact");
+
   });
 });
