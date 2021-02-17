eed57a04f8d8ac36803b0f4955d116f48482b32a
Eddine Omar <eddine.farid@hotmail.fr>
2021-02-17 22:20:17 +0100

    remove initial funding

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const ether = 10**18; // 1 ether = 1000000000000000000 wei
-
+const wei = 1;
 var SimpleBank = artifacts.require("SimpleBank");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleBank, { value: 30 * ether });
+  deployer.deploy(SimpleBank);
 };
