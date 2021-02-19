120493df17f29649213f52960f6bb9597aa327bb
Eddine Omar <eddine.farid@hotmail.fr>
2021-02-19 22:09:12 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,53 +1,45 @@
 var Web3 = require('web3');
+var Contract = require('web3-eth-contract');
 const fs = require('fs');
 const web3 = new Web3("http://localhost:9545")
 
 let rawdata = fs.readFileSync('build/contracts/SimpleBank.json');
 let simple_bank_abi = JSON.parse(rawdata);
+console.log()
 
 const ether = 10 ** 18;
 const wei = 1;
 const initialDepositsBalance = 15 * ether;
-simple_bank_address = "0x1b9F1C0Dda842151De212b5f9296588361C674B9"
-const SimpleBank = new web3.eth.Contract([simple_bank_abi], simple_bank_address)
 
 async function main() {
+    simple_bank_address = "0xCC663FBe6Ba8409F8fD676c61945cc158Cc311a5"
+    chairman_key = "0xa1fe98ac230a75831e7d380165e82fe7d2fcd653832c2763af096dd39470fdb5"
+    const SimpleBank = new web3.eth.Contract(simple_bank_abi["abi"], simple_bank_address)
+
     let accounts = await web3.eth.getAccounts();
     const chairperson = accounts[0];
     const alice = accounts[1];
 
     const tx = SimpleBank.methods.enroll(alice);
-    const gas = await tx.estimateGas({ from: alice });
+    // var gas = await tx.estimateGas({ from: alice });
     const gasPrice = await web3.eth.getGasPrice();
-    const data = tx.encodeABI();
-    const nonce = await web3.eth.getTransactionCount(address);
-
+    const abi_encoded = tx.encodeABI();
+    const nonce = await web3.eth.getTransactionCount(chairperson);
+    console.log("none", nonce)
+    data =  {
+        from: chairperson,
+        to: simple_bank_address,
+        data: abi_encoded,
+        gas: web3.utils.toHex("420000"),
+        // gas,
+        gasPrice : gasPrice,
+        nonce
+    }
     const signedTx = await web3.eth.accounts.signTransaction(
-        {
-            to: simple_bank_address,
-            data,
-            gas,
-            gasPrice,
-            nonce
-        },
-        "d698aa087c2f57993ec8dd0e25788583360eebea2b0d0aad27d648ed1bad75ca"
+       data,
+        chairman_key
     );
-    // console.log(`Old data value: ${await myContract.methods.data().call()}`);
-    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-    // console.log(`Transaction hash: ${receipt.transactionHash}`);
-    // console.log(`New data value: ${await myContract.methods.data().call()}`);
-
-    // const createTransaction = await web3.eth.accounts.signTransaction(
-    //     {
-    //         from: chairperson,
-    //         gas: 100000
-    //     },
-    //     "5362df8541ca60a07aa771ac0e194332498ca08ae5df53c15a820488a5409eb2"
-    // );
-
-    // await SimpleBank.methods.enroll(alice).send();
-    // await SimpleBank.deposit({ from: alice, value: web3.utils.toBN(deposit) });
-
+    const receipt = web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 }
 
 main()
