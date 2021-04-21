be2c1ce988fd635e05df15674dbc747d918823d8
Eddine Omar <eddine.farid@hotmail.fr>
2021-04-21 15:26:43 +0200

    update truffle config, use env

--- truffle-config.js
+++ truffle-config.js
@@ -47,7 +47,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
-    //   provider: () => new HDWalletProvider("73630578a4cf7b0c4d65929733f714ddb9119cd7798fec7bca8e19d0bea806bc", `https://ropsten.infura.io/v3/847eb8e2713c43d59dea835ceb49b39f`),
+    //   provider: () => new HDWalletProvider(privateKey, providerUrl),
     //   network_id: 3,       // Ropsten's id
     //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
     //   confirmations: 2,    // # of confs to wait between deployments. (default: 0)
